9c2e2488622859e3149a742ca2069390f203f1c2
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2022-07-09 11:52:12 -0400

    ajustes

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,46 @@
       <p class="subtitle has-text-centered">Mis trabajos</p>
       <div class="columns is-multiline">
         <div class="column is-4">
-          <div class="notification is-twitter">Twitter</div>
+          <div class="card">
+            <div class="card-image">
+              <b-image src="https://picsum.photos/id/1074/800/400"
+              placeholder="/assets/imgs/movieHero.jpg"
+              ratop="2by1"></b-image>
+            </div>
+            <div class="card-content">
+              <p class="title">Inicia el contenido</p>
+            </div>
+          </div>
         </div>
         <div class="column is-4">
-          <div class="notification is-red">Red</div>
+          <div class="card">
+            <div class="card-image">
+              <b-image src="/static/product.png" ratio="2by1"></b-image>
+            </div>
+            <div class="card-content">
+              <p class="title">Inicia el contenido</p>
+            </div>
+          </div>
         </div>
         <div class="column is-4">
-          <div class="notification is-primary">Primary</div>
+          <div class="card">
+            <div class="card-image">
+                <b-image
+                src="https://picsum.photos/id/1062/800/400"
+                ratio="2by1"
+                >
+                <template #placeholder>
+                    <b-skeleton
+                        class="skeleton-placeholder"
+                        height="100%"
+                    ></b-skeleton>
+                </template>
+                </b-image>
+            </div>
+            <div class="card-content">
+              <p class="title">Inicia el contenido</p>
+            </div>
+          </div>
         </div>
         <div class="column is-4">
           <div class="notification is-danger">Danger</div>
