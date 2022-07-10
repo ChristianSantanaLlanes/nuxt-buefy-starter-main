66017837a47630365bf36e45264629ebccdc8177
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2022-07-10 10:43:31 -0400

    arreglando los trabajos

--- components/Page/Card.vue
+++ components/Page/Card.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="card">
         <div class="card-image">
-            <b-image ratio="5by3" :src="item.foto" placeholder="https://ar.pinterest.com/pin/251638697914794219/https://i.pinimg.com/originals/b8/3e/c9/b83ec9d8ac7a6f2dfaa93fa4f150e3b6.gifhttps://i.pinimg.com/originals/76/47/bb/7647bb7539382dd708010625c4598021.gif"></b-image>
+            <b-image ratio="1by1" :src="item.foto" :placeholder="item.foto">
+            </b-image>
         </div>
         <div class="card-content">
             <nuxt-link class="subtitle" :to="{name:'detail', params:item}">{{item.name}}</nuxt-link>
@@ -24,5 +25,4 @@
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     <section class="section">
       <p class="title has-text-centered">Mis trabajos</p>
       <div class="columns is-multiline">
-        <div v-for="(item, i) in trabajos" :key="i" class="column is-4">
+        <div v-for="(item, i) in trabajos" :key="i" class="column is-3">
             <PageCard :item="item"/>
         </div>
       </div>
@@ -20,12 +20,12 @@ export default {
     data() {
         return {
             trabajos: [
-                { foto: "https://picsum.photos/600/400", name: "Producto 1", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
-                { foto: "https://picsum.photos/600/400", name: "Producto 2", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
-                { foto: "https://picsum.photos/600/400", name: "Producto 3", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
-                { foto: "https://picsum.photos/600/400", name: "Producto 4", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
-                { foto: "https://picsum.photos/600/400", name: "Producto 5", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
-                { foto: "https://picsum.photos/600/400", name: "Producto 6", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/product.png", name: "Producto 1", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/p1.png", name: "Producto 2", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/p2.png", name: "Producto 3", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/p3.png", name: "Producto 4", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/p1.png", name: "Producto 5", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
+                { foto: "/p2.png", name: "Producto 6", descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eos autem! Eos, dolor, alias culpa ipsam earum repellat ipsum, fugit ab hic veritatis accusantium cupiditate atque magnam deleniti. Modi, molestias?' },
             ]
         };
     },
